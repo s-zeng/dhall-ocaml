f1543c4e4ab1b94a7902935876b341a2079b187d
Simon Zeng <contact@simonzeng.com>
2023-01-13 18:24:30 -0500

    Working nix shell

--- shell.nix
+++ shell.nix
@@ -1,12 +1,30 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz") {} }:
 
+# pkgs.mkShell {
+#   nativeBuildInputs = [ pkgs.opam ];
+#   buildInputs = with pkgs; [
+#     dune_3
+#     ocaml
+#     ocamlPackages.core
+#     ocamlPackages.ppx_jane
+#     ocamlPackages.higher_kinded
+#   ];
+# }
+
 pkgs.mkShell {
-  packages = with pkgs; [
+  buildInputs = with pkgs; [
     dune_3
     ocaml
+    ocamlPackages.angstrom
+    ocamlPackages.bignum
     ocamlPackages.core
-    ocamlPackages.ppx_jane
+    ocamlPackages.findlib
     ocamlPackages.higher_kinded
+    ocamlPackages.ppx_jane
+  ];
+  packages = with pkgs; [
+    ocamlPackages.ocaml-lsp
+    ocamlformat
   ];
 }
 
